aaa4f75e03e6cee3408e7e8155117d137562e707
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 17:08:53 +0000

    Implimented Concern for Managing Image Data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,18 +1,19 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   # Demonstration Purposes
   def self.angular
     where(subtitle: 'Angular')
   end
   #-----------------------
-  
+
   scope :portfolio_post, -> {where(subtitle: 'My great service')}
-  
+
   # Sets default images
   after_initialize :set_defaults
-  
+
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
