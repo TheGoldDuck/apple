2a73aa7d1b47a97aa2353f39b4eb3c5e6db41c3a
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 15:36:15 +0000

    Integrated Technologies Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,3 +55,11 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+      name: "Technology #{technology}"
+    )
+  end
+
+puts "3 Technologies created"
