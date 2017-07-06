2bfd4e69ab19bf2b15db8a667704b67c02970674
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 17:11:20 +0000

    Implemented View Helper for Auth Links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Sign up",  new_user_registration_path) +
+      "<br>".html_safe + # Temporary
+      (link_to "Login", new_user_session_path)
+      # Ruby tends to return the last item.
+      # This is the solution to returning more than one thing.
+    end
+  end
 end
