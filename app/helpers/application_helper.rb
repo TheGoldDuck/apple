67a92f3c22a8f469d5c13a438c5e0052aabee441
loneengineer <hunteresorensen@gmail.com>
2017-07-13 16:56:36 +0000

    Updated Master Layout Styles Along with Helper Methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to "Sign up",  new_user_registration_path) +
-      "<br>".html_safe + # Temporary
-      (link_to "Login", new_user_session_path)
+      (link_to "Sign up",  new_user_registration_path, class: style) +
+      " ".html_safe + # Temporary
+      (link_to "Login", new_user_session_path, class: style)
       # Ruby tends to return the last item.
       # This is the solution to returning more than one thing.
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
