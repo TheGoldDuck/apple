927ce53c630d9e3d8e8552202a9ac5c500c62caf
loneengineer <hunteresorensen@gmail.com>
2017-07-13 14:26:21 +0000

    Implemented Authorization Engine for Portfolio Items and Blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
-    else
+    if current_user.is_a?(GuestUser)
       (link_to "Sign up",  new_user_registration_path) +
       "<br>".html_safe + # Temporary
       (link_to "Login", new_user_session_path)
       # Ruby tends to return the last item.
       # This is the solution to returning more than one thing.
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete
     end
   end
 
