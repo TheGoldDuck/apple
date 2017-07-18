fc54c26e1b92e470c6b93366faacd163972b28e0
loneengineer <hunteresorensen@gmail.com>
2017-07-18 15:36:25 +0000

    Implemented Partials for Blog, Font Awesome, And Pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,9 +6,9 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
-    ###
-    ##
+    @blogs = Blog.page(params[:page]).per(5)
+    ### Refer to bottom
+    ##  of page
     @page_title = "My Portfolio Blog"
   end
 
